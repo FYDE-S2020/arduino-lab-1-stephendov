bd0c788cc96e9b2d8e83adcc8467372b9bc82943
stephendov <40470740+stephendov@users.noreply.github.com>
2020-02-07 21:51:32 -0600

    Wrote dimmer function

--- Lab1_Blink/Lab1_Blink.ino
+++ Lab1_Blink/Lab1_Blink.ino
@@ -4,11 +4,24 @@
   Turns an LED on for one second, then off for one second, repeatedly.
 */
 int interval = 250;
+int freq = 100;
+
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void dimmer(int freq, int duty) {
+  int period, onTime, offTime;
+  period = 1000/freq;
+  onTime = period * duty / 100;
+  offTime = period - onTime;
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(onTime);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(offTime);
 }
 
 void timedBlink(int offTime) {
@@ -20,7 +33,10 @@ void timedBlink(int offTime) {
 
 // the loop function runs over and over again forever
 void loop() {
- timedBlink(interval);
- timedBlink(interval*2);
- timedBlink(interval*4);
+  for (int i=0; i <=100; i++){
+  dimmer(freq, i);
+  }
+  for (int j=100; j >=0; j= j-1){
+  dimmer(freq, j);
+  }
 }
